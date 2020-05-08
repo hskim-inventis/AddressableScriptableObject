8a89ffda7f58c2933e52bba0c9c873daec0c9f49
hskim-inventis <63832665+hskim-inventis@users.noreply.github.com>
2020-05-09 00:19:25 +0900

    첫 커밋

---
+++ Assets/GalleryScriptableObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/GalleryScriptableObject", order = 1)]
+public class GalleryScriptableObject : ScriptableObject
+{
+    public string caption;
+
+    public Sprite spThumbnail;
+    public VideoClip clipVideo;
+    public Sprite spLarge;
+}
---
+++ Assets/ScriptableAssetLoader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class ScriptableAssetLoader : MonoBehaviour
+{
+    public Image imgThumbnail;
+    public TextMeshProUGUI tmpCaption;
+    public VideoPlayer vp;
+    public RawImage ri;
+
+    public AssetReference arKSP;
+    public AssetReferenceT<GalleryScriptableObject> artKSP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arKSP.LoadAssetAsync<GalleryScriptableObject>().Completed += ScriptableAssetLoader_Completed;
+    }
+
+    private void ScriptableAssetLoader_Completed(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<GalleryScriptableObject> obj)
+    {
+        Debug.Log($"Status {obj.Status} caption {obj.Result.caption}");
+        tmpCaption.text = obj.Result.caption;
+        imgThumbnail.sprite = obj.Result.spThumbnail;
+
+        vp.clip = obj.Result.clipVideo;
+        vp.Prepare();
+        vp.prepareCompleted += Vp_prepareCompleted;
+    }
+
+    /*
+    IEnumerator PlayVideo(VideoClip clip)
+    {
+        vp.clip = clip;
+        vp.Prepare();
+        vp.prepareCompleted += Vp_prepareCompleted;       
+    }
+    */
+    private void Vp_prepareCompleted(VideoPlayer source)
+    {
+        ri.texture = vp.texture;
+        vp.Play();
+    }
+}
